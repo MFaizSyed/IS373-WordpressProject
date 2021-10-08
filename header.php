0ac6efd832a7ad418ed40b8d8449d167bd125e27
MFaizSyed <72081457+MFaizSyed@users.noreply.github.com>
2021-10-07 21:21:55 -0400

    feat: header.php and index.php files

---
+++ header.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>Tutorial theme</title>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/jquery.js'; ?>">
+</script>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/jquery-ui.min.js'; ?>">
+</script>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/bootstrap.js'; ?>">
+</script>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/css/bootstrap.css'; ?>">
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+
+<body>
+
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">Swords</a></li>
+  <li><a href="#contact">Ancient Temples</a></li>
+  <li><a href="#about">Cuisine</a></li>
+</ul>
+
+
+<div id="ttr_header" class="jumbotron">
+<h1></h1>
+</div>
+<div class="container">
